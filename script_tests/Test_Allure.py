921104bc8ba44e4fb7d9a0474a2a80eda756cd74
2278966523@qq.com <071254cheng>
2020-11-26 15:59:43 +0800

    test

--- script_tests/Test_Allure.py
+++ script_tests/Test_Allure.py
@@ -7,8 +7,3 @@ class Test_Allure:
     def test_001(self):
         allure.attach("描述", "测试显示")
         assert 0
-
-#
-# if __name__ == '__main__':
-#     pytest.main(["-s", "--alluredir", "report", '17_allure.py'])
-#     # allure generate report/ -o report/html
